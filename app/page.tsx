550196a0674e840ba37cbe551b3377273815a8e6
oluwaseunladeinde <oluwaseun.ladeinde@yahoo.com>
2024-11-25 17:13:42 +0100

    chore: implement design system, theme and tailwind config

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="flex items-center justify-center h-screen bg-amber-100">
       <h1 className="text-3xl">NexustoreIt - The only storage solution you need</h1>
 
     </div>
